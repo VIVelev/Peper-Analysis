f38bbdf3491bf64b941071667812e91582840c66
Victor Velev <velev.victor@yahoo.com>
2019-01-14 23:48:02 +0200

    [Fix] typo puct -> punkt

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-pipenv run python -c 'import nltk; nltk.download("stopwords"); nltk.download("punct"); nltk.download("wordnet");'
+pipenv run python -c 'import nltk; nltk.download("stopwords"); nltk.download("punkt"); nltk.download("wordnet");'
